f1d26fb274693b1634e6b877c8a8a9b1a8f68407
USER <yesithasathsaraa@gmail.com>
2020-08-09 11:10:39 +0530

    Created UI and Controller

---
+++ V1/src/lk/ijse/choolSystem/view/Controller/StudentFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.choolSystem.view.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class StudentFormController {
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAge;
+    public TextField txtAddress;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAge;
+    public TableColumn colAddress;
+
+    public void DeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void UpdateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void SaveOnAction(ActionEvent actionEvent) {
+    }
+}
